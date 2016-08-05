ba86a5387af1dd76a496037053b578cf2fd22d5b
Martin Alef <Martin Alef>
2016-08-05 12:43:24 +0200

    design changes

--- modules/subskill/subskill-dir.js
+++ modules/subskill/subskill-dir.js
@@ -1,6 +1,6 @@
 var subskillTemplate = `
 <div class="col-xs-6">{{subskill.name}}</div>
-<div class="col-xs-3"><img class="skillImage{{subskill.rating}}"/></div>
+<div class="col-xs-3"><img class="skillImage{{subskill.rating}}" alt="{{subskill.rating}}"/></div>
 <div class="col-xs-3 ">{{subskill.experience}}</div>`;
 
 
