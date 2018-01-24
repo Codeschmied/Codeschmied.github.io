801d86f8902033a2d0ce506ecbfc090ec03c4661
MONOLYTH\Martin <croben@outlook.com>
2018-01-24 18:39:08 +0100

    Fix for printer logo

--- modules/project/project-dir.js
+++ modules/project/project-dir.js
@@ -1,8 +1,8 @@
 var projectTemplate = `<div class="row">
-<h4 class="col-xs-12 col-sm-2 visible-xs" style="word-wrap: break-word">{{project.PrimaryTitle}}</h4>
-<h3 class="col-xs-12 col-sm-2  hidden-xs" style="word-wrap: break-word">{{project.PrimaryTitle}}</h3>
-<h4 ng-if="project.SecondaryTitle" class="col-xs-12 col-sm-10 visible-xs">{{project.SecondaryTitle}}</h4>
-<h3 ng-if="project.SecondaryTitle" class="col-xs-12 col-sm-10  hidden-xs">{{project.SecondaryTitle}}</h4>
+<h3 class="col-xs-12 col-sm-2 visible-xs" style="word-wrap: break-word">{{project.PrimaryTitle}}</h4>
+<h2 class="col-xs-12 col-sm-2  hidden-xs" style="word-wrap: break-word">{{project.PrimaryTitle}}</h3>
+<h3 ng-if="project.SecondaryTitle" class="col-xs-12 col-sm-10 visible-xs">{{project.SecondaryTitle}}</h4>
+<h2 ng-if="project.SecondaryTitle" class="col-xs-12 col-sm-10  hidden-xs">{{project.SecondaryTitle}}</h4>
 </div>
 <div class="row">
   <div class="col-xs-3 col-sm-2">Zeitraum</div>
@@ -14,17 +14,17 @@ var projectTemplate = `<div class="row">
 </div>
 <div class="row">
   <div class="col-sm-2 hidden-xs">Tätigkeit und Aufgaben</div>
-  <h5 class="col-xs-12 visible-xs">Tätigkeit und Aufgaben:</h5>
+  <h4 class="col-xs-12 visible-xs">Tätigkeit und Aufgaben:</h5>
   <div class="col-xs-9 col-sm-10">{{project.Activity}}</div>
 </div>
 <div class="row">
 <div class="col-sm-2 hidden-xs">Technologien</div>
-<h5 class="col-xs-12 visible-xs">Technologien:</h5>
+<h4 class="col-xs-12 visible-xs">Technologien:</h5>
   <div class="col-xs-12 col-sm-10">{{project.Technologies}}</div>
 </div>
 <div class="row" ng-if="project.Details">
 <div class="col-sm-2 hidden-xs">Besonderheiten</div>
-<h5 class="col-xs-12 visible-xs">Besonderheiten:</h5>
+<h4 class="col-xs-12 visible-xs">Besonderheiten:</h5>
   <div class="col-xs-12 col-sm-10">{{project.Details}}</div>
 
 
