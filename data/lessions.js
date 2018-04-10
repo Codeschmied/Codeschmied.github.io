f8ae9a7bc941943b1e9401d0ee5281e323c679f7
MONOLYTH\Martin <croben@outlook.com>
2018-04-10 13:05:22 +0200

    Fix an embarassing typo

--- data/lessions.js
+++ data/lessions.js
@@ -1,8 +1,8 @@
-angular.module('profile')
+﻿angular.module('profile')
         .constant('lessions',[
   {year:"2015", topic: "AngularJS.DE, zweitägig: AngularJS und Javascript Intensiv-Workshop"},
   {year:"2015", topic: "Javascript Days 2015, dreitägig: Ecmascript 6, Hoodie, Grunt, Javascript Security"},
-  {year:"2011", topic: "Complement, zweitägig: SQL Server"},
-  {year:"2011", topic: "Complement, zweitägig: Neues in .NET 4.0"},
-  {year:"2010", topic: "Complement, dreitägig: Objektorientierte Programmierung C# .NET 3.5"}
+  {year:"2011", topic: "Conplement, zweitägig: SQL Server"},
+  {year:"2011", topic: "Conplement, zweitägig: Neues in .NET 4.0"},
+  {year:"2010", topic: "Conplement, dreitägig: Objektorientierte Programmierung C# .NET 3.5"}
 ]);
