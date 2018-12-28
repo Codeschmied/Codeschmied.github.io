15b4a38568bf793045c51746a3683aba83556e13
Monolyth\Martins Workshop <alef@freenet.de>
2018-12-28 17:41:54 +0100

    Exact dates

--- data/lessions.js
+++ data/lessions.js
@@ -1,5 +1,6 @@
 ﻿angular.module('profile')
         .constant('lessions',[
+  {year: "2018", topic: "Nürnberg Digital: IoT entdecken @ Paessler, BURGER, BLECH UND BLOCKCHAIN (Neue Materialien Fürth), Digitale Transformation (Datev) " },
   {year:"2015", topic: "AngularJS.DE, zweitägig: AngularJS und Javascript Intensiv-Workshop"},
   {year:"2015", topic: "Javascript Days 2015, dreitägig: Ecmascript 6, Hoodie, Grunt, Javascript Security"},
   {year:"2011", topic: "Conplement, zweitägig: SQL Server"},
