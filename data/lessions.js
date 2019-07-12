09a2b80ee96a3e65185e687ed4408a9692baee64
Monolyth\Martins Workshop <alef@freenet.de>
2019-07-12 17:16:58 +0200

    Fixed h-tags, fixed responsive styles, fixed content

--- data/lessions.js
+++ data/lessions.js
@@ -1,5 +1,7 @@
 ﻿angular.module('profile')
         .constant('lessions',[
+        
+  {year: "2019", topic: "Developer Week Nürnberg, zweitägig. Unter Anderem: DDD, blazor & web assembly, .NET core, docker für .NET core, entity framework core 3.0" },
   {year: "2018", topic: "Nürnberg Digital: IoT entdecken @ Paessler, BURGER, BLECH UND BLOCKCHAIN (Neue Materialien Fürth), Digitale Transformation (Datev) " },
   {year:"2015", topic: "AngularJS.DE, zweitägig: AngularJS und Javascript Intensiv-Workshop"},
   {year:"2015", topic: "Javascript Days 2015, dreitägig: Ecmascript 6, Hoodie, Grunt, Javascript Security"},
